cfd55f59aea40f87834673dcf4b9b72e8f05994f
professor-oats <chronophob@gmail.com>
2024-11-29 00:47:17 +0100

    working adw

--- NinjaAcademy/index.js
+++ NinjaAcademy/index.js
@@ -64,23 +64,30 @@ const keys= {
   },
   d: {
     pressed: false,
+  },
+  w: {
+    pressed: false,
   }
 }
+let lastKey
 
 function animate() {
+  // Loop over itself to continue to request animation frames
   window.requestAnimationFrame(animate);
+  // Fill background black
   ctx.fillStyle = 'black'
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   player.update();
   enemy.update();
 
   // Default player velocity here
   player.velocity.x = 0
 
-  if (keys.a.pressed) {
+  if (keys.a.pressed && lastKey === 'a') {
     player.velocity.x = -1
   }
-  else if (keys.d.pressed) {
+  else if (keys.d.pressed && lastKey === 'd') {
     player.velocity.x = 1
   }
 }
@@ -91,9 +98,14 @@ window.addEventListener('keydown', (event) => {
   switch (event.key) {
     case 'd':
       keys.d.pressed = true;
+      lastKey = 'd';
       break;
     case 'a':
       keys.a.pressed = true;
+      lastKey = 'a';
+      break;
+    case 'w':
+      player.velocity.y = -10
       break;
   }
   console.log(event.key);
@@ -106,6 +118,9 @@ window.addEventListener('keyup', (event) => {
       break;
     case 'a':
       keys.a.pressed = false;
+      break;
+    case 'w':
+      keys.w.pressed = false;
       break;
   }
   console.log(event.key);
