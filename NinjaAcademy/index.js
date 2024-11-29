829fa0971185ff391ccc620d99f16ec7baa82e93
professor-oats <chronophob@gmail.com>
2024-11-29 03:23:28 +0100

    starting with collisions

--- NinjaAcademy/index.js
+++ NinjaAcademy/index.js
@@ -9,16 +9,32 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7
 
 class Sprite {
-  constructor({ position, velocity }) {
+  constructor({ position, velocity, color = 'red' }) {
     this.position = position;
     this.velocity = velocity;
+    this.width = 50;
     this.height = 150;
     this.lastKey = null;
+    this.attackBox = {
+      position: this.position,
+      width: 100,
+      height: 50,
+    };
+    this.color = color
   }
 
   draw() {
-    ctx.fillStyle = 'red'
-    ctx.fillRect(this.position.x, this.position.y, 50, this.height)
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    // attackbox
+    ctx.fillStyle = 'green'
+    ctx.fillRect(
+      this.attackBox.position.x,
+      this.attackBox.position.y,
+      this.attackBox.width,
+      this.attackBox.height
+    )
   }
 
   // Method responsible for what updates to do on animation frame
@@ -55,7 +71,8 @@ const enemy = new Sprite({
   velocity: {
     x: 0,
     y: 0
-  }
+  },
+  color: 'blue'
 });
 
 const keys= {
@@ -108,6 +125,15 @@ function animate() {
   else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
     enemy.velocity.x = 5
   }
+
+  // detect for collision
+  if (
+    player.attackBox.position.x + player.attackBox.width >=
+    enemy.position.x && player.attackBox.position.x <= enemy.position.x + enemy.width
+  ) {
+    console.log("hit")
+  }
+
 }
 
 animate();
