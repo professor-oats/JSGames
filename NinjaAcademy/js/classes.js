710ae9a6386c7641b025ce7968d72653e8d39290
professor-oats <chronophob@gmail.com>
2024-12-06 14:08:21 +0100

    starting with sprites

---
+++ NinjaAcademy/js/classes.js
@@ -0,0 +1,82 @@
+class Sprite {
+  constructor({ position, imageSrc }) {
+    // We will use the imageSrc to render a sprite for the frames
+    this.position = position;
+    this.width = 50;
+    this.height = 150;
+    this.image = new Image()
+    this.image.src = imageSrc;
+  };
+
+  draw() {
+    // using position.x and position.y is posible since we pass
+    // position as an object with assign values for these
+    ctx.drawImage(this.image, this.position.x,  this.position.y);
+  }
+
+  // Method responsible for what updates to do on animation frame
+  update() {
+    this.draw()
+  }
+}
+
+class Fighter {
+  constructor({ position, velocity, color = 'red', offset }) {
+    this.position = position;
+    this.velocity = velocity;
+    this.width = 50;
+    this.height = 150;
+    this.lastKey = null;
+    this.attackBox = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      offset: offset,
+      width: 100,
+      height: 50,
+    };
+    this.color = color;
+    this.isAttacking;
+    this.health = 100;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    // attackbox
+    if (this.isAttacking) {
+      ctx.fillStyle = 'green'
+      ctx.fillRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height
+      )
+    }
+  }
+
+  // Method responsible for what updates to do on animation frame
+  update() {
+    this.draw();
+    this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+    this.attackBox.position.y = this.position.y;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0;
+    }
+    else {
+      this.velocity.y += gravity;
+    }
+  }
+
+  attack() {
+    this.isAttacking = true;
+    setTimeout(() => {
+      this.isAttacking = false;
+    }, 100)
+  }
+}
